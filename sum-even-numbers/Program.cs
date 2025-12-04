97d1f2d95d4d8db630e4ad743c9604ad122e3e64
fiorenzaa <fiorenza.laila24@gmail.com>
2025-12-04 14:43:44 +0700

    Membuat program dan running

---
+++ sum-even-numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿static int SumEven()
+{
+  int sum = 0;
+
+  for (int i = 0; i <= 100; i++)
+    if (i % 2 == 0)
+      sum += i;
+
+  return sum;
+}
+
+Console.WriteLine($"Jumlah bilangan genap 1-100: {SumEven()}");
+
